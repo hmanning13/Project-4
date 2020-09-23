a35b6dc0c5a5b34712d9d3b30ee62c0b8cd53512
Hayden Manning <67125970+hmanning13@users.noreply.github.com>
2020-09-23 16:11:23 -0400

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -8,5 +8,17 @@
         this.phrase = phrase.toLowerCase();
      }
 
-     
+    addPhraseToDisplay() {
+        const ul = document.querySelector("#phrase ul");
+        let html = "";
+        for(let i = 0; i < this.phrase.length; i++) {
+            if (this.phrase[i] === " ") {
+                html += `<li class = "space"> </li>`;
+            } else {
+                html += `<li class = "hide letter ${this.phrase[i]}">
+                ${this.phrase[i]}</li>`;
+            }
+        }
+        ul.append(html);
+    }; 
  }
