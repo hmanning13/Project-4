d337cec179b5596d6d51d67d6d15b5c0aaa555aa
Hayden Manning <67125970+hmanning13@users.noreply.github.com>
2020-09-29 15:55:58 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@ startBttn.addEventListener("click", () => {
     game = new Game();
     game.startGame();
 });
+
+
+const keys = document.getElementById("qwerty");
+keys.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        game.handleInteraction(event.target);
+    }
+});
